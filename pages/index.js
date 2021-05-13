ba0a6257325d4c3ebe4db969824397537051f30a
kimo070 <kimovschaik@gmail.com>
2021-05-13 21:12:57 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Navbar from './navbar'
+
+export default function Home() {
+  return (
+    <div style={{ maxWidth: `960px`, margin: `0 auto` }}>
+      <Navbar />
+      <h1>Main page</h1>
+    </div>
+  )
+}
